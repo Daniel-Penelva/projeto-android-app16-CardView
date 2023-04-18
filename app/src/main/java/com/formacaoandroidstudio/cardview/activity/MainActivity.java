832bbd89c1ebb94c0835d1c53c865b0f073a169e
Daniel Penelva <d4n.andrade@gmail.com>
2023-04-18 03:38:23 -0300

    CardView Finalizado| Criando Classe Postagem | impl. AdapterPostagem

--- app/src/main/java/com/formacaoandroidstudio/cardview/activity/MainActivity.java
+++ app/src/main/java/com/formacaoandroidstudio/cardview/activity/MainActivity.java
@@ -8,11 +8,18 @@ import android.os.Bundle;
 
 import com.formacaoandroidstudio.cardview.R;
 import com.formacaoandroidstudio.cardview.adapter.PostagemAdapter;
+import com.formacaoandroidstudio.cardview.model.Postagem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     // Criando o objeto da Classe
    private RecyclerView recyclerPostagem;
+
+   // Criando a lista de itens
+   List<Postagem> postagens = new ArrayList<Postagem>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +34,31 @@ public class MainActivity extends AppCompatActivity {
         recyclerPostagem.setLayoutManager(layoutManager);
 
         /* Definir o Adapter */
-        PostagemAdapter adapter = new PostagemAdapter();
+        prepararPostagens();
+        PostagemAdapter adapter = new PostagemAdapter( postagens );
         recyclerPostagem.setAdapter(adapter);
 
     }
+
+    public void prepararPostagens(){
+        Postagem p;
+
+        p = new Postagem("Daniel Penelva", "Viagem Legal!", R.drawable.imagem1);
+        this.postagens.add(p);
+
+        p = new Postagem("Hotel JM", "Viaje, aproveite nosso desconto!", R.drawable.imagem2);
+        this.postagens.add(p);
+
+        p = new Postagem("Maria Luiza", "Paris!", R.drawable.imagem3);
+        this.postagens.add(p);
+
+        p = new Postagem("Marcos Paulo", "Que foto Linda!", R.drawable.imagem4);
+        this.postagens.add(p);
+    }
 }
+
+/* Anotações:
+*
+* Em attributes para o componente Button. Para adicionar icone ou imagem a um button, basta
+* buscar o atributo 'drawable<posição>'
+* */
--- app/src/main/java/com/formacaoandroidstudio/cardview/adapter/PostagemAdapter.java
+++ app/src/main/java/com/formacaoandroidstudio/cardview/adapter/PostagemAdapter.java
@@ -1,5 +1,6 @@
 package com.formacaoandroidstudio.cardview.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,26 +9,47 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.formacaoandroidstudio.cardview.R;
+import com.formacaoandroidstudio.cardview.model.Postagem;
+
+import java.util.List;
+
 /* Adapter e o ViewHolder. Essas duas classes trabalham juntas para definir como seus dados são exibidos.*/
 
 public class PostagemAdapter extends RecyclerView.Adapter<PostagemAdapter.MyViewHolder> {
 
     /* Para implementar os métodos - clica com o botão direito do mouse em 'generate' e 'implements methods' */
 
+    private List<Postagem> listaPostagens;
+
+    public PostagemAdapter(List<Postagem> postagens) {
+        this.listaPostagens = postagens;
+    }
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+         View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.postagem_detalhe, parent, false);
+
+        return new MyViewHolder(itemLista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
+        Postagem postagem = listaPostagens.get(position);
+
+        // Usando o holder para setar os valores do componentes
+        holder.nome.setText(postagem.getNome());
+        holder.postagem.setText(postagem.getPostagem());
+        holder.imagem.setImageResource(postagem.getImagem());
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listaPostagens.size();
     }
 
     /* Os dados que serão ecxibidos */
@@ -37,8 +59,13 @@ public class PostagemAdapter extends RecyclerView.Adapter<PostagemAdapter.MyView
         private TextView postagem;
         private ImageView imagem;
 
-        public MyViewHolder() {
+        public MyViewHolder(View itemView) {
             super(itemView);
+
+            //capturar os ids dos componentes-  usa-se o mesmo itemView.
+            nome = itemView.findViewById(R.id.textNome);
+            postagem = itemView.findViewById(R.id.textPostagem);
+            imagem = itemView.findViewById(R.id.imagePostagem);
         }
     }
 }
